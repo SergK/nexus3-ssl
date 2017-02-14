71da3682babd027838bec91b3677653b512b864d
Sergey Kulanov <skulanov@mirantis.com>
2017-02-14 13:41:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nexus3-ssl
 Docker compose, nexus3 with ssl support
+
+
+Put `ssl.key` and `ssl.crt` files in in nginx/ssl/ directory
+
+Use ./manage.sh
+
---
+++ manage.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e
+
+IN_OPERATION="${1}"
+BASE_DIR=$(dirname $(readlink -f "${0}"))
+DOCKER_COMPOSE_CONF="${BASE_DIR}/docker-compose.yaml"
+
+source "${BASE_DIR}/env.config"
+
+
+function init_env () {
+  sudo mkdir -p "${VOLUME_PATH}"
+  sudo chown 200:200 -R "${VOLUME_PATH}"
+
+  sed -i "s#{{SERVER_NAME}}#${SERVER_NAME}#g" "${BASE_DIR}/nginx/nginx.conf"
+}
+
+
+function check_ssl_key () {
+  if [[ -f "${BASE_DIR}/nginx/ssl/ssl.key"  &&  -f "${BASE_DIR}/nginx/ssl/ssl.crt" ]]; then
+    echo "SSL keys file found, continue"
+  else
+    echo "No ssl files found. Exiting"
+    exit 1
+  fi
+}
+
+
+case "${IN_OPERATION}" in
+
+    status)
+        docker-compose -f "${DOCKER_COMPOSE_CONF}" ps
+    ;;
+
+    debug)
+        init_env
+        check_ssl_key
+        docker-compose -f "${DOCKER_COMPOSE_CONF}" up
+    ;;
+
+    start)
+        init_env
+        check_ssl_key
+        docker-compose -f "${DOCKER_COMPOSE_CONF}" up -d --no-recreate
+    ;;
+
+    stop)
+        docker-compose -f "${DOCKER_COMPOSE_CONF}" down
+    ;;
+
+    *)
+        cat << EOF
+Usage: $0 ACTION
+
+ ACTION:
+   status               get containers status
+   debug                run docker-compose in foreground
+   start                start all containers in background
+   stop                 stop all containers
+EOF
+        exit 1
+    ;;
+esac
